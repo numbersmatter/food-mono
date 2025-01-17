cdd47158b1d3ff72a10aa5937841dc1aa2a76d5a
MarkMutt <lleasy8@gmail.com>
2025-01-17 12:31:07 -0500

    adding environment setup

--- apps/staff-apps/semester-app/app/services/firestore.server.ts
+++ apps/staff-apps/semester-app/app/services/firestore.server.ts
@@ -1,3 +1,16 @@
 import { initializeFirestoreFoodPantryDb } from '@food-mono/firedb';
+import { getServerEnv } from '~/env.server';
 
-const { foodPantryDb } = initializeFirestoreFoodPantryDb({});
+const { SERVICE_ACCOUNT, FIREBASE_APP_NAME } = getServerEnv();
+
+const { foodPantryDb } = initializeFirestoreFoodPantryDb({
+  FIREBASE_APP_NAME,
+  SERVICE_ACCOUNT,
+  collectionPaths: {
+    users: 'users',
+    applications: 'applications',
+    events: 'events',
+    registrations: 'registrations',
+    reservations: 'reservations',
+  },
+});
